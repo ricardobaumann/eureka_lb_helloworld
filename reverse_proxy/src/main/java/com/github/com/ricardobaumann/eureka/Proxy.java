9a25340e4ba413d23eb1f020ac5a774634a00475
ricardobaumann <ricardo.luis.baumann@gmail.com>
2016-10-12 15:35:48 +0200

    reverse proxy uo and running

--- reverse_proxy/src/main/java/com/github/com/ricardobaumann/eureka/Proxy.java
+++ reverse_proxy/src/main/java/com/github/com/ricardobaumann/eureka/Proxy.java
@@ -3,8 +3,8 @@ package com.github.com.ricardobaumann.eureka;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by ricardobaumann on 11/10/16.
  */
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableZuulProxy
 @RestController
-@EnableResourceServer
 public class Proxy {
 
     public static void main(String[] args) {
