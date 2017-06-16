12969438534499cc94d8f3948cc6ea0c4a091892
Ricardo Baumann <gemini.richard@gmail.com>
2017-06-16 13:01:02 +0200

    archetype wip

---
+++ spring.cloud.archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,27 @@
+package ${archetype.groupId};
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+/**
+ * Hello world!
+ */
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+@EnableHystrix
+@EnableCircuitBreaker
+@RestController
+public class App {
+    public static void main(final String[] args) {
+        // Tell server to look for registration.properties or registration.yml
+        System.setProperty("spring.config.name", "appname");
+
+        SpringApplication.run(App.class, args);
+    }
+}
